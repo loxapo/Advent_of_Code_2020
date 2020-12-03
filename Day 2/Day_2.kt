3855d30f9bc283a05a27c423ef51a5226f30a6e5
unknown <paolo.fiorilli27@gmail.com>
2020-12-03 02:10:14 -0500

    Day 2 part 2

--- Day 2/Day_2.kt
+++ Day 2/Day_2.kt
@@ -6,7 +6,8 @@ fun main(args: Array<String>)
     val inputStream: InputStream = File("C:\\Users\\Paolo\\VSCODE\\Advent_of_Code_2020\\Day 2\\Day_2_Input.txt").inputStream()
     val lineList = ArrayList<String>()
     inputStream.bufferedReader().forEachLine{lineList.add(it)} 
-    var count = 0
+    var count1 = 0
+    var count2 = 0
     val delimiter1 = " "
     val delimiter2 = "-"
     val delimiter3 = ": "
@@ -15,8 +16,17 @@ fun main(args: Array<String>)
         var parts = lineList.get(i).split(delimiter1, delimiter2, delimiter3)
         if (parts.get(parts.lastIndex).filter{it == parts.get(parts.lastIndex-1).get(0)}.count() in parts.get(0).toInt()..parts.get(1).toInt())
         {
-            count++
+            count1++
+        }
+        var password = parts.get(parts.lastIndex)
+        var ind1 = parts.get(0).toInt()-1
+        var ind2 = parts.get(1).toInt()-1
+        var char = parts.get(2).get(0)
+        if ((password.get(ind1)==char && password.get(ind2)!=char)||(password.get(ind1)!=char && password.get(ind2)==char))
+        {
+            count2++
         }
     }
-    println("The number of valid passwords is: " + count)
+    println("Part 1: The number of valid passwords is: " + count1)
+    println("Part 2: The number of valid passwords is: " + count2)
 }
